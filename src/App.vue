14c55db5f74491c9d4b5159768e6a1da354d4b80
Nonog-narlux342 <68525348+Nonog-narlux342@users.noreply.github.com>
2022-09-24 14:25:44 +0700

    count

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Profile" class="rounded-circle" src="./assets/1.jpg" Width="20%"/>
+    <h1>Nonog_Narlux</h1>
+    <h3>Images Score</h3>
+    <hr />
+    <div class="row">
+      <div class="col">
+        <img alt="Profile" class="rounded" src="./assets/3.jpg" Width="80%"/>
+        <br />
+        <br />
+        <HelloWorld />
+      </div>
+      <div class="col">
+        <img alt="Profile" class="rounded" src="./assets/5.jpg" Width="80%"/>
+        <br />
+        <br />
+        <HelloWorld />
+      </div>
+      <div class="col">
+        <img alt="Profile" class="rounded" src="./assets/6.jpg" Width="80%"/>
+        <br />
+        <br />
+        <HelloWorld />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      total: 0,
+      name: "",
+      code: "",
+      gpa: 0.1,
+    };
+  },
+};
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,25 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="row-12">
+      <button @click="Like()" class="btn btn-outline-info">
+        <i class="bi bi-emoji-laughing-fill" aria-hidden="true"></i> Like
+      </button>
+      <button @click="Dislike()" class="btn btn-outline-info">
+        <i class="bi bi-emoji-frown-fill" aria-hidden="true"></i> Dislike
+      </button>
+      <button @click="Love()" class="btn btn-outline-info">
+        <i class="bi bi-suit-heart-fill" aria-hidden="true"></i> Love
+      </button>
+      <button @click="Sad()" class="btn btn-outline-info">
+        <i class="bi bi-emoji-expressionless-fill"></i> Sad
+      </button>
+      <button @click="reset()" class="btn btn-outline-info">
+        <i class="bi bi-x-circle-fill" aria-hidden="true"></i> reset
+      </button>
+      <br />
+      <br />
+      <p v-bind:style="{fontSize: fonts + 'px' }">{{ counter }}</p>
+    </div>
   </div>
 </template>
 
@@ -35,7 +28,36 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data: function () {
+    return {
+      counter: 0,
+      fonts: 50,
+    };
+  },
+  methods: {
+    Like: function () {
+      this.counter += 1;
+      console.log(this.counter);
+    },
+    Dislike: function () {
+      this.counter -= 5;
+      console.log(this.counter);
+    },
+    Love: function () {
+      this.counter += 5;
+      console.log(this.counter);
+    },
+    Sad: function () {
+      this.counter -= 1;
+      console.log(this.counter);
+    },
+    reset: function () {
+      this.counter = 0;
+      console.log(this.counter);
+    },
+  }, 
+  
 }
 </script>
 
